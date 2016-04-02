f6a1ebf39da94c7ae9c488ab29d82f099b3ac771
amykyttc <amykyttc@gmail.com>
2016-04-03 01:39:58 +0300

    spring mvc xml-config

---
+++ src/main/java/com/fidel/recognizer/controller/ImageController.java
@@ -0,0 +1,28 @@
+package com.fidel.recognizer.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class ImageController {
+
+    String message = "Welcome to Spring MVC!";
+
+    @RequestMapping("/")
+    public String home() {
+        return "redirect:/hello";
+    }
+
+    @RequestMapping("/hello")
+    public ModelAndView showMessage(
+            @RequestParam(value = "name", required = false, defaultValue = "World") String name) {
+        System.out.println("in controller");
+
+        ModelAndView mv = new ModelAndView("hello");
+        mv.addObject("message", message);
+        mv.addObject("name", name);
+        return mv;
+    }
+}
